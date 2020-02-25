ef6c06d998e353435240f1893348050544727a4e
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-25 12:30:00 +0530

    Static buttons added
    TO sign up page and login page.

--- app/src/main/java/com/hashedin/mainassignment/LogIn.kt
+++ app/src/main/java/com/hashedin/mainassignment/LogIn.kt
@@ -1,12 +1,16 @@
 package com.hashedin.mainassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class LogIn : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
     }
 }
