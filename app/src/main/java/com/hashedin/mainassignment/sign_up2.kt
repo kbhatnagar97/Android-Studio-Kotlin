334787d5bb1e5d0cd68b857edbb6ff95a8d9e49d
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-25 19:14:29 +0530

    Signup page 2 pixel perfect
    Interactive ui made with button click listener
    implemented to all 3 pages

--- app/src/main/java/com/hashedin/mainassignment/sign_up2.kt
+++ app/src/main/java/com/hashedin/mainassignment/sign_up2.kt
@@ -1,17 +1,12 @@
 package com.hashedin.mainassignment
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUp : AppCompatActivity() {
+class sign_up2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-
-        
+        setContentView(R.layout.activity_sign_up2)
     }
 }
