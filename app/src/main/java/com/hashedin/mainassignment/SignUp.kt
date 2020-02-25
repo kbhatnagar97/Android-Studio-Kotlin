8ad2dccde488ebf9abfe23c8ae729c8d439f435b
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-25 23:00:33 +0530

    OTP generated
    APIserives
    RetroClass
    On Generate Post request sent to server

--- app/src/main/java/com/hashedin/mainassignment/SignUp.kt
+++ app/src/main/java/com/hashedin/mainassignment/SignUp.kt
@@ -3,8 +3,12 @@ package com.hashedin.mainassignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUp : AppCompatActivity() {
 
@@ -12,13 +16,28 @@ class SignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        val mob = mobile_number.text.toString()
+
         button1.setOnClickListener()
         {
-
+            val activityHandler = RetroClass().getAPIInstance()
+            val getToken = activityHandler.usersignup("phone", mob)
+            getToken.enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    if (response.isSuccessful) {
+                        Log.e("Successful", "OTP Sent")
+                    }
+                    else
+                    {
+                        Log.e("Failed", "OTP was not sent")
+                    }
+                }
+            })
         }
 
         button2.setOnClickListener()
         {
+
 
             if(true)
             {
