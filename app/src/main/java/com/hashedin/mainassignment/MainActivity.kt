c1227e7b01fe6d9067616246f8d8de2b1280b060
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-25 11:46:57 +0530

    Page Viewr
    page viewr functionality added to welcome page.

--- app/src/main/java/com/hashedin/mainassignment/MainActivity.kt
+++ app/src/main/java/com/hashedin/mainassignment/MainActivity.kt
@@ -2,12 +2,18 @@ package com.hashedin.mainassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var images: Array<Int> = arrayOf(R.drawable.carousel1,R.drawable.carousel2,R.drawable.carousel3)
+    var adapter: PagerAdapter = SliderAdapter(this, images)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login1)
+        setContentView(R.layout.activity_main)
 
+        viewpager.adapter = adapter
     }
 }
