5491f3b5724a6b9570f0742cacc711c5836590e1
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-26 06:52:53 +0530

    Buttons working on Login page with toast functionality
    Toast
    Directivity to login page
    directivity to sign up page

--- app/src/main/java/com/hashedin/mainassignment/sign_up2.kt
+++ app/src/main/java/com/hashedin/mainassignment/sign_up2.kt
@@ -1,12 +1,26 @@
 package com.hashedin.mainassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up2.*
 
 class sign_up2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up2)
+
+        back_button.setOnClickListener()
+        {
+            val intent = Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+
+        text2.setOnClickListener()
+        {
+            val intent = Intent(this,LogIn::class.java)
+            startActivity(intent)
+        }
     }
 }
